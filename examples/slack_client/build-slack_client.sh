d152eb70068bf365978de2e8edd6a6ce2262fb48
Nilton Oliveira <jniltinho@gmail.com>
2022-05-02 19:22:24 -0300

    Create build-slack_client.sh

---
+++ examples/slack_client/build-slack_client.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/jniltinho/docker-pyinstaller.git
+cd docker-pyinstaller/examples/slack_client
+chmod +x data/build.sh
+docker run --rm -ti -v $(pwd)/data:/data jniltinho/pyinstaller exec ./build.sh
