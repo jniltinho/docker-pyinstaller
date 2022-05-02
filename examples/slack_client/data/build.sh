24a12b8fc33a043864c41a3efcbb7a15744bc493
Nilton Oliveira <jniltinho@gmail.com>
2022-05-02 19:20:01 -0300

    Create build.sh

---
+++ examples/slack_client/data/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## docker run --rm -ti -v $(pwd)/data:/data jniltinho/pyinstaller exec ./build.sh
+
+pip3 --no-cache-dir install -r requirements.txt
+rm -rf slack_client dist *.spec build dist *pycache*
+pyinstaller --onefile ./slack_client.py
+mv dist/slack_client . ;rm -rf *.spec build dist *pycache*
